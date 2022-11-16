6c8f32e41568aad5418e8a063bfee838e1b1c1c4
Ochieng Otieno <ochiengotieno71@gmail.com>
2022-11-16 10:39:12 +0300

    feat: add recommended maintenance times based on 'daily, weekly, monthly, and 3 months' interval

--- app/models/service.rb
+++ app/models/service.rb
@@ -33,12 +33,25 @@ class Service < ApplicationRecord
       number_of_failures += count if code != '200'
     end
 
-    total_operational_hours = (report_period * 24.0) - ((number_of_failures * service_check_interval) / 60.0)
+    total_operational_hours_daily = (report_period * 24.0) - ((number_of_failures * service_check_interval) / 60.0)
+    total_operational_hours_weekly = (report_period * 24.0 * 7.0) - ((number_of_failures * service_check_interval) / 60.0)
+    total_operational_hours_monthly = (report_period * 24.0 * 28.0) - ((number_of_failures * service_check_interval) / 60.0)
+    total_operational_hours_3_monthly = (report_period * 24.0 * 28.0 * 3) - ((number_of_failures * service_check_interval) / 60.0)
+
     return unless number_of_failures.positive?
 
-    mean_time_between_failures = total_operational_hours / number_of_failures
+    mean_time_between_failures_daily = total_operational_hours_daily / number_of_failures
+    mean_time_between_failures_weekly = total_operational_hours_weekly / number_of_failures
+    mean_time_between_failures_monthly = total_operational_hours_monthly / number_of_failures
+    mean_time_between_failures_3_monthly = total_operational_hours_3_monthly / number_of_failures
 
-    next_maintenance_time = Time.now + (mean_time_between_failures - 1).hours
+    next_maintenance_time_daily = Time.now + (mean_time_between_failures_daily - 1).hours
+    next_maintenance_time_weekly = Time.now + (mean_time_between_failures_weekly - 1).hours
+    next_maintenance_time_monthly = Time.now + (mean_time_between_failures_monthly - 1).hours
+    next_maintenance_time_3_monthly = Time.now + (mean_time_between_failures_3_monthly - 1).hours
+
+    maintenance_times = [next_maintenance_time_daily, next_maintenance_time_weekly, next_maintenance_time_monthly,
+                         next_maintenance_time_3_monthly]
   end
 
   # after_initialize do
