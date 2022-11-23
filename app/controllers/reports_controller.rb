4588660b5883b73502f3e468d4cdf58de9c65d6d
Ochieng Otieno <ochiengotieno71@gmail.com>
2022-11-23 15:02:19 +0300

    style: improve report summary form

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,20 +1,33 @@
 class ReportsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_service, only: %i[create destroy]
 
   def index
     @reports = Report.all
   end
 
   def create
-    @service = Service.find(params[:service_id])
-    @report = @service.reports.create(params[:report].permit(:maintainer, :reason))
+    @report = @service.reports.create(report_params)
     redirect_to maintenance_action_path(@service)
   end
 
   def destroy
-    @service = Service.find(params[:service_id])
     @report = @report.find(params[:id])
     @report.destroy
     redirect_to maintenance_action_path(@service)
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:maintainer, :reason, :duration)
+  end
+
+  def set_service
+    @service = Service.find(params[:service_id])
+  end
+
+  def set_report
+    @report = Report.find(params[:id])
   end
 end
